209d2a00df357ec5f8f124372cc38d3b3729b40a
Erico Alexandre <ericoabs25@gmail.com>
2021-10-03 01:08:51 -0300

    Criação do component de botão

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import { ButtonHTMLAttributes } from 'react';
+import * as Styled from './styles';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const Button = (props: ButtonProps) => {
+  return <Styled.Container {...props} />;
+};
