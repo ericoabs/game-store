aa2d69d1f279acd5b6055ca3ade4435ab1c475bc
Erico Alexandre <ericoabs25@gmail.com>
2021-10-03 01:20:45 -0300

    Mapeando os dados

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
 import { data } from '../api/mock';
+import { Game } from '../components/Game';
 
 console.log(data);
 
 export const Home = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      {data.map((game) => {
+        return (
+          <Game
+            key={game.id}
+            id={game.id}
+            name={game.name}
+            price={game.price}
+            score={game.score}
+            image={game.image}
+          />
+        );
+      })}
+    </div>
+  );
 };
