e83b077265df16543d4efb0162c74a29cc311d88
Erico Alexandre <ericoabs25@gmail.com>
2021-10-03 01:36:14 -0300

    Recuperando as imagens do cloudinary

--- src/api/mock.js
+++ src/api/mock.js
@@ -4,62 +4,71 @@ export const data = [
     name: 'Super Mario Odyssey',
     price: 197.88,
     score: 100,
-    image: 'super-mario-odyssey.png',
+    image:
+      'https://res.cloudinary.com/dvawcyisr/image/upload/v1624885661/super-mario-odyssey_pjdx0y.png',
   },
   {
     id: 201,
     name: 'Call Of Duty Infinite Warfare',
     price: 49.99,
     score: 80,
-    image: 'call-of-duty-infinite-warfare.png',
+    image:
+      'https://res.cloudinary.com/dvawcyisr/image/upload/v1624885660/call-of-duty-infinite-warfare_pv2pnx.png',
   },
   {
     id: 102,
     name: 'The Witcher III Wild Hunt',
     price: 119.5,
     score: 250,
-    image: 'the-witcher-iii-wild-hunt.png',
+    image:
+      'https://res.cloudinary.com/dvawcyisr/image/upload/v1624885661/the-witcher-iii-wild-hunt_t700fm.png',
   },
   {
     id: 99,
     name: 'Call Of Duty WWII',
     price: 249.99,
     score: 205,
-    image: 'call-of-duty-wwii.png',
+    image:
+      'https://res.cloudinary.com/dvawcyisr/image/upload/v1624885661/call-of-duty-wwii_ivjzce.png',
   },
   {
     id: 12,
     name: 'Mortal Kombat XL',
     price: 69.99,
     score: 150,
-    image: 'mortal-kombat-xl.png',
+    image:
+      'https://res.cloudinary.com/dvawcyisr/image/upload/v1624885662/mortal-kombat-xl_zvcj09.png',
   },
   {
     id: 74,
     name: 'Shards of Darkness',
     price: 71.94,
     score: 400,
-    image: 'shards-of-darkness.png',
+    image:
+      'https://res.cloudinary.com/dvawcyisr/image/upload/v1624885661/shards-of-darkness_gznbzb.png',
   },
   {
     id: 31,
     name: 'Terra Média: Sombras de Mordor',
     price: 79.99,
     score: 50,
-    image: 'terra-media-sombras-de-mordor.png',
+    image:
+      'https://res.cloudinary.com/dvawcyisr/image/upload/v1624885662/terra-media-sombras-de-mordor_drbb2t.png',
   },
   {
     id: 420,
     name: 'FIFA 18',
     price: 195.39,
     score: 325,
-    image: 'fifa-18.png',
+    image:
+      'https://res.cloudinary.com/dvawcyisr/image/upload/v1624885660/fifa-18_ddjlkp.png',
   },
   {
     id: 501,
     name: 'Horizon Zero Dawn',
     price: 115.8,
     score: 290,
-    image: 'horizon-zero-dawn.png',
+    image:
+      'https://res.cloudinary.com/dvawcyisr/image/upload/v1624885660/horizon-zero-dawn_t887ex.png',
   },
 ];
