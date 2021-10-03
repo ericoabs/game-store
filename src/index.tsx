ab272269b2234587ae301741423c54d50a012137
Erico Alexandre <ericoabs25@gmail.com>
2021-10-03 01:03:24 -0300

    Configuração de fontes e estilos globais

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Home } from './pages/Home';
+import { GlobalStyles } from './styles/global-styles';
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
+    <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root'),
 );
