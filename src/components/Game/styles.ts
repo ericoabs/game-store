529d7d83ffa177514a6fdb8e617293b029209016
Erico Alexandre <ericoabs25@gmail.com>
2021-10-03 01:30:29 -0300

    Estilizando o Card dos jogos

--- src/components/Game/styles.ts
+++ src/components/Game/styles.ts
@@ -1,3 +1,24 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.34);
+  border-radius: 0.6rem;
+
+  img {
+    width: 15rem;
+    height: 15rem;
+  }
+  ul {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+    width: 100%;
+    padding: 1rem;
+    list-style: none;
+  }
+`;
