ff1f8dc70de00648b2fb7063cffa0aed45175b0b
Erico Alexandre <ericoabs25@gmail.com>
2021-10-04 17:38:28 -0300

    Criando o select do filtro

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,23 +2,43 @@ import { data } from '../../api/mock';
 import { Game } from '../../components/Game';
 import * as Styled from './styles';
 
-console.log(data);
-
 export const Home = () => {
   return (
-    <Styled.Container>
-      {data.map((game) => {
-        return (
-          <Game
-            key={game.id}
-            id={game.id}
-            name={game.name}
-            price={game.price}
-            score={game.score}
-            image={game.image}
-          />
-        );
-      })}
-    </Styled.Container>
+    <>
+      <span>
+        <label id="filter">Filtrar</label>
+        <select name="filter" id="filter">
+          <option value="" disabled defaultValue="Alphabetically">
+            Selecione uma opção...
+          </option>
+          <option value="Alphabetically" label="Alfabeticamente">
+            Alfabeticamente
+          </option>
+          <option value="SmallerPrice" label="Menor Preço">
+            Menor Preço
+          </option>
+          <option value="BiggerPrice" label="Maior Preço">
+            Maior Preço
+          </option>
+          <option value="Score" label="Nota">
+            Nota
+          </option>
+        </select>
+      </span>
+      <Styled.Container>
+        {data.map((game) => {
+          return (
+            <Game
+              key={game.id}
+              id={game.id}
+              name={game.name}
+              price={game.price}
+              score={game.score}
+              image={game.image}
+            />
+          );
+        })}
+      </Styled.Container>
+    </>
   );
 };
